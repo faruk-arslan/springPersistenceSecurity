8f8ea7b12be7af0b301376d0e2b119c601c512d0
Faruk Arslan <farukarslan011@gmail.com>
2021-05-14 10:38:31 +0300

    Review for beans-initial commit

---
+++ src/main/java/com/udacity/jwdnd/c1/review/OtherBeans.java
@@ -0,0 +1,21 @@
+package com.udacity.jwdnd.c1.review;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class OtherBeans {
+
+    @Bean
+    public String upperCaseMessage(MessageService messageService){
+        System.out.println("Creating upperCaseMessage Bean...");
+        return messageService.returnUpperCase();
+    }
+    @Bean
+    public String lowerCaseMessage(MessageService messageService){
+        System.out.println("Creating lowerCaseMessage Bean...");
+        return messageService.returnLowerCase();
+    }
+}
